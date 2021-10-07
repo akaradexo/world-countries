7e0d19152cdd7278c226cd51daa6e14587ab96fc
Ashutosh Karade <69238714+xR0v3Rx@users.noreply.github.com>
2021-10-07 22:29:57 +0530

    population done

--- script/script.js
+++ script/script.js
@@ -5,9 +5,6 @@ const topPopulation = document.getElementById("population")
 const topLanguage = document.getElementById("language")
 const btnPopulation = document.getElementById("btnPopulation")
 const btnLanguage = document.getElementById("btnLanguage")
-const populationName = document.getElementById("populationName")
-const populationChart = document.getElementById("populationChart")
-const populationNumber = document.getElementById("populationNumber")
 
 
 
@@ -17,11 +14,14 @@ xValues.forEach((element,index) => {
 let populationFiltered =  countries.filter((function(o) { return o.population === xValues[index]; }))
 
 populationFiltered.forEach((value,index)=>{
-   populationName.innerHTML =`<li>${value.name}</li>`
-   let width = 100 - ((value.population)/763165599)*100 
-   populationChart.innerHTML =`<li style="width:${width}%;background-color:red"></li>`
-   populationNumber.innerHTML =`<li>${value.population}</li>`
-   topPopulation.appendChild(populationNumber,populationChart,populationNumber);
+  let width = Math.abs(((value.population)/7742800000)*100)
+  const listItem = document.createElement('ul');
+  listItem.innerHTML=`
+  <li class="name">${value.name}</li>
+  <li class="bar" style="width:${width}rem;background-color:red"></li>
+  <li lass="numbers">${value.population}</li>`
+
+   topPopulation.appendChild(listItem);
 
   });
 
@@ -31,11 +31,15 @@ populationFiltered.forEach((value,index)=>{
 
 
 
-const showPopulation = function(){
+const showPopulation = function(e){
+  e.preventDefault();
   topPopulation.style.display="flex"
   topLanguage.style.display="none"
+  
 }
-const showLanguage = function(){
+const showLanguage = function(e){
+  e.preventDefault();
+
   topPopulation.style.display="none"
   topLanguage.style.display="flex"
 }
